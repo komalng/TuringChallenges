79c4889e586b4f9d9bfb5e86944dfc1553620d78
divyaparmar18 <divya18@navgurukul.org>
2019-09-19 16:11:46 +0530

    Add message

--- server.js
+++ server.js
@@ -3,11 +3,14 @@ const app = express();
 const department = require("./controller/tableDetails");
 const singleDepartment = require("./controller/singleDepartment");
 const singleCategory = require("./controller/singleCategory");
+const newCustomer = require("./controller/newCustomer");
 
 
 app.get("/:tableDetails",department.details);
 app.get("/departments/:id",singleDepartment.single);
 app.get("/category/:id",singleCategory.single);
+app.post("/api/customer",newCustomer.customer);
+
 
 
 app.listen(2000,()=>{console.log("Express is listening port number 2000")})
